b2ea51e877e87fb37a72aa50c1090e3e0a8d1d89
badwulf51 <aronomalley@yahoo.com>
2018-11-30 20:31:12 +0000

    Have the model testing the mnist dataset

--- recognition.py
+++ recognition.py
@@ -10,38 +10,56 @@ from keras.models import model_from_json # importing json for keras
 def NeuralNetwork() :
     model = kr.models.Sequential() # create a neural network first and then build it by layers
 
-    # Add a hidden layer with 1000 neurons and an input layer with 784.
+    # Adds hidden layer with 100 neurons 
     model.add(kr.layers.Dense(units=600, activation='linear', input_dim=784))
     model.add(kr.layers.Dense(units=400, activation='relu'))
-# Add a three neuron output layer.
+# three neuron output layer.
     model.add(kr.layers.Dense(units=10, activation='softmax'))
 
 # Build the graph.
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    with gzip.open('MNISTdata/train-images-idx3-ubyte.gz', 'rb') as f:
+    with gzip.open('MNISTdata/train-images-idx3-ubyte.gz', 'rb') as f: # opens zipped training images folder
      trainimg = f.read()
 
-    with gzip.open('MNISTdata/train-labels-idx1-ubyte.gz', 'rb') as f:
+    with gzip.open('MNISTdata/train-labels-idx1-ubyte.gz', 'rb') as f:  # opens zipped train labels folder 
      trainlbl = f.read()
 
-     print ("keras version: " + kr.__version__)
+    trainimg = ~np.array(list(trainimg[16:])).reshape(60000, 28, 28).astype(np.uint8) / 255.0 # division by 255 
+    trainlbl =  np.array(list(trainlbl[ 8:])).astype(np.uint8) # sets the trainlbls
+
+    inputs = trainimg.reshape(60000, 784) # reshapes
+    # encodes the bytes to binary 
+    encoder = pre.LabelBinarizer()
+    encoder.fit(trainlbl)
+    outputs = encoder.transform(trainlbl)
+
+    #print(trainlbl[0], outputs[0]) this will test to make sure the concoder worked
+
+    model.fit(inputs, outputs, epochs=10, batch_size=100) # fits the model with the inputs and outputs, 10 epochs are used to see how tests differentate from eachother
+
+print ("keras version: " + kr.__version__)
 
 def Menu():
 
     
     
-    print("Welcome to the Digit Recognition Script!") # Headers and decorators 
-    print("Enter 1 to FEATURE COMING SOON") # Option to allow the user to choose an image for recognition testing 
-    print("Enter 2 to exit the program") # Exits the program 
+    print("Welcome to the Digit Recognition Script! ") # Headers and decorators 
+    print("Enter 1 to test the network with the MNIST dataset ") # Option to allow the user to choose an image for recognition testing 
+    print("Enter 2 to test the network with a choosen image ") # Option to allow the user to choose an image for recognition testing 
+    print("Enter 3 to exit the program ") # Exits the program 
 
-    choice = input ("Please make a choice: ") # Allows the user to enter his/her options
+    choice = input ("Input: ") # Allows the user to enter his/her options
 
-    if choice == "2": # if choice 2 is entered user exits program 
-        SystemExit
+    if choice == "1": # if choice one is enterted the model will be tested with the MNIST dataset
+        NeuralNetwork()
+    
+    elif choice == "3":
+        SystemExit # exits the system 
+        
     
 def main(): # main function 
-   NeuralNetwork()
+   Menu()
 
 
 
