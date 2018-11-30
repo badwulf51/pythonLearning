6203db3f3422248ed8d50ef16de2cd7e0c0f4cf8
badwulf51 <aronomalley@yahoo.com>
2018-11-30 00:54:59 +0000

    added start to keras script

---
+++ recognition.py
@@ -0,0 +1,7 @@
+import numpy as np
+import keras as kr
+
+print ("keras version: " + kr.__version__) # outputs the keras version number you have installed, used to see if everything is working fine
+#def sayhello() : 
+ #   print ("this is cool")
+  #  print ("Hello python")
